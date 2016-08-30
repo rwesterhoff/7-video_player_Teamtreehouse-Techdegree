5619f001c946e1ea78025af1d7cc4df6c8dcd1bc
Robin Westerhoff <rwesterhoff@me.com>
2016-08-30 19:37:48 +0200

    Chrome optimized player
    Everything works as it should in Chrome

--- js/video-player.js
+++ js/video-player.js
@@ -12,6 +12,8 @@ https://davidwalsh.name/html5-video-duration
 
 var supportsVideo = !!document.createElement('video').canPlayType;
 var videoContainer = document.querySelector('#video-container');
+var videoPlayer = document.querySelector('#video-player');
+var contentContainer = document.querySelector('#content');
 
 if (supportsVideo) {
 
@@ -19,27 +21,33 @@ if (supportsVideo) {
 
     // Define custom controls
     var divHTML = document.createElement('div');
-    var playButtonHTML = '<button id="playpause" type="button" data-state="play">Play/Pause</button>\n';
-    var stopButtonHTML = '<button id="stop" type="button" data-state="stop">Stop</button>\n';
+    var playButtonHTML = '<button id="playpause" class="srt" type="button" data-state="play">Play/Pause</button>\n';
+    var playBackSliderHTML = '<input type="range" id="speed-bar" min="0.5" max="2" step="0.25" value="1">\n';
+    var playBackIndicatorHTML = '<button id="playback-indicator">' + videoPlayer.playbackRate + 'x' + '</button>';
+    var stopButtonHTML = '<button id="stop" class="srt" type="button" data-state="stop">Stop</button>\n';
     var currentTimeIndicatorHTML = '<span id="current-time-indicator"></span>\n';
     var durationIndicatorHTML = '<span id="duration-indicator"></span>\n';
     var timeHTML = '<div id="time-indicator">' + currentTimeIndicatorHTML + ' / ' + durationIndicatorHTML + '</div>';
-    var progressBarHTML = '<div class="progress">\n' + '<progress id="progress" value="0" min="0">\n' + '<span id="progress-bar"></span>\n' + '</progress>\n' + '</div>\n';
-    var muteButtonHTML = '<button id="mute" type="button" data-state="mute">Mute/Unmute</button>\n';
+    var progressBarHTML = '<div id="progress-container" class="progress">\n' + '<span id="progress-bar"></span>\n' + '</span>\n' + '<span id="buffer-bar"></span>\n' + '</span>\n' + '</div>\n';
+    var muteButtonHTML = '<button id="mute" class="srt" type="button" data-state="mute">Mute/Unmute</button>\n';
     var volumeSliderHTML = '<input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1">\n';
-    var fullscreenButtonHTML = '<button id="full-screen" type="button" data-state="go-fullscreen">Fullscreen</button>\n';
+    var fullscreenButtonHTML = '<button id="full-screen" class="srt" type="button" data-state="go-fullscreen">Fullscreen</button>\n';
     var captionsButtonHTML = '<button id="captions" type="button" data-state="captions">CC</button>\n';
 
     // Make controls HTML ready
     var controlsHTML = '';
     controlsHTML += progressBarHTML;
+    controlsHTML += '<div id="button-container">';
     controlsHTML += playButtonHTML;
-    controlsHTML += stopButtonHTML;
     controlsHTML += timeHTML;
-    controlsHTML += muteButtonHTML;
-    controlsHTML += volumeSliderHTML;
-    controlsHTML += fullscreenButtonHTML;
+    controlsHTML += stopButtonHTML;
+    controlsHTML += playBackSliderHTML;
+    controlsHTML += playBackIndicatorHTML;
     controlsHTML += captionsButtonHTML;
+    controlsHTML += volumeSliderHTML;
+    controlsHTML += muteButtonHTML;
+    controlsHTML += fullscreenButtonHTML;
+    controlsHTML += '</div>';
 
     //Make controls container ready
     divHTML.setAttribute('id', 'video-controls');
@@ -51,7 +59,6 @@ if (supportsVideo) {
     videoContainer.appendChild(divHTML);
 
     // Hide the default controls
-    var videoPlayer = document.querySelector('#video-player');
     videoPlayer.controls = false;
 
     // Display the user defined video controls
@@ -59,16 +66,21 @@ if (supportsVideo) {
     videoControls.style.display = 'block';
 
     // Bind the buttons
+    // var buttonContainer = document.querySelector('#button-container');
     var playButton = document.querySelector('#playpause');
+    var playBackSlider = document.querySelector('#speed-bar');
     var stopButton = document.querySelector('#stop');
     var muteButton = document.querySelector('#mute');
     var volumeSlider = document.querySelector('#volume-bar');
+    var progressContainer = document.querySelector('#progress-container');
     var progress = document.querySelector('#progress');
     var progressBar = document.querySelector('#progress-bar');
+    var bufferBar = document.querySelector('#buffer-bar');
     var fullscreenButton = document.querySelector('#full-screen');
     var captionsButton = document.querySelector('#captions');
 
     //Indicators
+    var playBackIndicator = document.querySelector('#playback-indicator');
     var currentTimeIndicator = document.querySelector('#current-time-indicator');
     var durationIndicator = document.querySelector('#duration-indicator');
 
@@ -78,53 +90,57 @@ if (supportsVideo) {
     playButton.addEventListener('click', function(e) {
         if (videoPlayer.paused || videoPlayer.ended) {
             videoPlayer.play();
+            this.setAttribute('data-state', 'playing');
         } else {
             videoPlayer.pause();
+            this.setAttribute('data-state', 'paused');
         }
+    });
+
+    // Event listener for the playback speed
+    playBackSlider.addEventListener("change", function() {
+        // Update the video volume
+        videoPlayer.playbackRate = playBackSlider.value;
+        playBackIndicator.innerText = videoPlayer.playbackRate + 'x';
     });
 
     // Event listener for the stop button
     stopButton.addEventListener('click', function(e) {
         // Stop is not possible on a video element so we play pretend
+        playButton.setAttribute('data-state', 'paused');
         videoPlayer.pause();
         videoPlayer.currentTime = 0;
-        progress.value = 0;
     });
 
     // Event listener for the mute button
     muteButton.addEventListener('click', function(e) {
         // Toggle mute
-        videoPlayer.muted = !videoPlayer.muted;
-        /*        if (videoPlayer.muted) {
-        videoPlayer.setAttribute("disabled", "disabled");
-
+        if (videoPlayer.muted) {
+            videoPlayer.muted = false;
+            this.setAttribute('data-state', 'sound');
         } else {
-        volumeSlider.removeAttribute("disabled");
-        }*/
+            videoPlayer.muted = true;
+            this.setAttribute('data-state', 'muted');
+        }
     });
 
     // Event listener for the volume bar
     volumeSlider.addEventListener("change", function() {
         // Update the video volume
         videoPlayer.volume = volumeSlider.value;
-        /*        if (videoPlayer.value === 0) {
-        videoPlayer.muted = true;
-        } else {
-        videoPlayer.muted = false;
-        }*/
     });
 
-    // Event listener for the progress bar
+    /*// Event listener for the progress bar
     videoPlayer.addEventListener('loadedmetadata', function() {
         // Set the max value
         progress.setAttribute('max', videoPlayer.duration);
-
-
-    });
+        buffer.setAttribute('max', videoPlayer.duration);
+    });*/
 
     // Event listener for the time indicator
-    var timeIndicator = setInterval(function() {
+    setInterval(function() {
             if (videoPlayer.readyState > 0) {
+
                 // Update video duration
                 var minutes = parseInt(videoPlayer.duration / 60, 10);
                 var seconds = Math.floor(videoPlayer.duration % 60);
@@ -133,6 +149,7 @@ if (supportsVideo) {
                 } else {
                     durationIndicator.innerText = minutes + ':' + seconds;
                 }
+
                 // Update time played
                 var minutesPlayed = parseInt(videoPlayer.currentTime / 60, 10);
                 var secondsPlayed = Math.floor(videoPlayer.currentTime % 60);
@@ -141,38 +158,42 @@ if (supportsVideo) {
                 } else {
                     currentTimeIndicator.innerText = minutesPlayed + ':' + secondsPlayed;
                 }
+
+                // Update video duration
+                var played = Math.floor((videoPlayer.currentTime / videoPlayer.duration) * 100);
+                progressBar.style.width = played + '%';
+
+                // Update buffer bar
+                var buffered = videoPlayer.buffered.end(videoPlayer.buffered.length - 1);
+                var bufferCalc = Math.floor((buffered / videoPlayer.duration) * 100);
+                bufferBar.style.width = (bufferCalc - played) + '%';
             }
         },
         200);
 
-    // Update playbar + played time
-    videoPlayer.addEventListener('timeupdate', function() {
-        // Update the playbar 
-        progress.value = videoPlayer.currentTime;
-        progressBar.style.width = Math.floor((videoPlayer.currentTime / videoPlayer.duration) * 100) + '%';
+    // Check if the browser is already in fullscreen mode
+    var isFullScreen = function() {
+        return !!(document.fullScreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement);
+    };
 
-    });
-
-    videoPlayer.addEventListener('timeupdate', function() {
-        //Solving mobile issues if it's not yet set
-        if (!progress.getAttribute('max')) {
-            progress.setAttribute('max', videoPlayer.duration);
+    // Skip ahead
+    progressContainer.addEventListener('click', function(e) {
+        var positionClicked = e.pageX;
+        var positionContent = contentContainer.offsetLeft;
+        var positionProgress = this.offsetLeft;
+        var playWidth = progressContainer.offsetWidth;
+        var videoDuration = videoPlayer.duration;
+        var multiPlier;
+        if (isFullScreen()) {
+            multiPlier = positionClicked / playWidth;
+        } else if (positionContent === 0) {
+            multiPlier = (positionClicked - positionProgress) / playWidth;
+        } else {
+            multiPlier = (positionClicked - positionContent - positionProgress) / playWidth;
         }
-        progress.value = videoPlayer.currentTime;
-        progressBar.style.width = Math.floor((videoPlayer.currentTime / videoPlayer.duration) * 100) + '%';
-    });
-
-    // Skip ahead
-    progress.addEventListener('click', function(e) {
-        var posClick = (e.pageX - this.offsetLeft) / this.offsetWidth;
-        videoPlayer.currentTime = posClick * videoPlayer.duration;
-    });
-
-
-    // Skip ahead
-    progress.addEventListener('click', function(e) {
-        var posClick = (e.pageX - this.offsetLeft) / this.offsetWidth;
-        videoPlayer.currentTime = posClick * videoPlayer.duration;
+        var setTime = multiPlier * videoDuration;
+        videoPlayer.currentTime = setTime;
+        // alert(setTime);
     });
 
     // Check if browser supports the Fullscreen API
@@ -183,10 +204,7 @@ if (supportsVideo) {
         fullscreenButton.style.display = 'none';
     }
 
-    // Check if the browser is already in fullscreen mode
-    var isFullScreen = function() {
-        return !!(document.fullScreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement);
-    };
+
 
     var setFullscreenData = function(state) {
         videoContainer.setAttribute('data-fullscreen', !!state);
@@ -245,6 +263,8 @@ if (supportsVideo) {
 
     // Creation menu with captions
     var captionMenuButtons = [];
+    var df = document.createDocumentFragment();
+    var captionsMenu = df.appendChild(document.createElement('ul'));
     var createMenuItem = function(id, lang, label) {
         var listItem = document.createElement('li');
         var button = listItem.appendChild(document.createElement('button'));
@@ -279,11 +299,10 @@ if (supportsVideo) {
     };
 
     // Reading textTracks properties and building the menu
-    var captionsMenu;
     if (videoPlayer.textTracks) {
         // Using document fragments for better performance as this is just in memory and not in DOM
-        var df = document.createDocumentFragment();
-        var captionsMenu = df.appendChild(document.createElement('ul'));
+        // var df = document.createDocumentFragment();
+        // var captionsMenu = df.appendChild(document.createElement('ul'));
         captionsMenu.className = 'captions-menu';
         captionsMenu.appendChild(createMenuItem('captions-off', '', 'Off'));
         for (var i = 0; i < videoPlayer.textTracks.length; i++) {
@@ -306,44 +325,72 @@ if (supportsVideo) {
 
 
     // Update the captions HTML with the default language captions
-
+    var captionsContainer = document.querySelector('#video-captions');
+    var captionsHTML = '';
     var trackElements = document.querySelectorAll("track");
     // for each track element
     for (var i = 0; i < trackElements.length; i++) {
-        if ( trackElements[i].hasAttribute("align") ) {
-            console.log(trackElements[i]);
-        }
-        /*var textTrack = this.track;
-        var isShowing = textTrack.mode === "showing";
-        if (isShowing) {
-            alert(textTrack[i] + ' is showing');
+        if (trackElements[i].hasAttribute("default")) {
+
             trackElements[i].addEventListener("load", function() {
-                    // for each cue
-                    for (var j = 0; j < textTrack.cues.length; ++j) {
-                        var cue = textTrack.cues[j];
-                        // do something
+                var textTrack = this.track;
+                // Set the paragraph
+                captionsHTML += '<p>';
+                // Get all the cues
+                for (var j = 0; j < textTrack.cues.length; ++j) {
+                    var cue = textTrack.cues[j];
+                    captionsHTML += '<a href="#" id="' + cue.id + '" class="caption-cue" data-state="inactive" data-start="' + cue.startTime + '">' + cue.text + ' ' + '</a>';
+                }
+                // Close the paragraph
+                captionsHTML += '</p>';
+                // Update '#video-captions' with captions from default VTT file
+                captionsContainer.innerHTML = captionsHTML;
+
+                // Each time a cue changes check it's ID
+                textTrack.oncuechange = function() {
+                    // "this" is a textTrack
+                    var cue = this.activeCues[0]; // assuming there is only one active cue
+                    // Check the captions 
+                    var captions = captionsContainer.getElementsByTagName("a");
+                    for (var i = 0; i < captions.length; i++) {
+                        // And see if the ID is similar to the active cue's ID
+                        if (captions[i].id === cue.id) {
+                            // Set the caption to 'active'
+                            captions[i].setAttribute('data-state', 'active');
+                        } else {
+                            // Or 'inactive'
+                            captions[i].setAttribute('data-state', 'inactive');
+                        }
+                    }
+                };
+                // Event listener for the time indicator
+                function setCaptionTriggers() {
+                    var captionTriggers = captionsContainer.getElementsByTagName("a");
+                    for (var i = 0; i < captionTriggers.length; i++) {
+                        // If a cue is clicked
+                        captionTriggers[i].addEventListener('click', function(e) {
+                            // Get the correct starting time
+                            var triggerStartTime = this.getAttribute('data-start');
+                            // Play video from here
+                            videoPlayer.currentTime = triggerStartTime;
+                            videoPlayer.play();
+                        });
                     }
                 }
-
-            }
-        }*/
+                setCaptionTriggers();
+            }, false);
+        }
     }
-
-    // Update '#video-captions' with captions from VTT file
-    // Get current time
-    // Add '.active-caption' to the current playing cue
-    // Remove class from all the others
-
-
-    // If a cue is clicked
-    //     Get the correct starting time
-    //     Play video from here
-
-    // EXCEEDS EXPECTATIONS:
-    //     Embed VTT file
-    //         Add CC button to toggle on/off
-    //     Creative responsive design
-    //     Playback speed control or other helpful controls
-    //     Volume control
-    //     Playback buffering indicator
 }
+
+/*TODO:
+JS-->
+if video is playing, make playbutton data-state 'playing' 
+clicking on captions in chrome will not play initially (probably no ID is set yet)
+Captions not loading in Safari and Firefox
+Video not well loaded initially in Safari
+CSS-->
+Style captions menu
+Fix bottom gap in figure element
+Style sliders
+*/
